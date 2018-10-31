6f55d7b630ee7086dbdc6e916d9b84fe48f01072
Jonathan <jonathan@iMac9.local>
2018-10-31 08:54:11 -0400

    Starting to write the program by using commands in string and list

---
+++ assignment_seven.py
@@ -0,0 +1,7 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def encode():
+    text = input("Please enter text to be encoded: ")
+    keyword = input("Please enter the key string: ")
+    text.lower().split(" ")
