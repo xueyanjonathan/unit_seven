d68f5911de80eb94ce96d522f4ad169a306384a0
Jonathan <jonathan@iMac9.local>
2018-11-01 10:13:26 -0400

    Using index to convert letters to numbers to get the converted text

--- assignment_seven.py
+++ assignment_seven.py
@@ -4,4 +4,13 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 def encode():
     text = input("Please enter text to be encoded: ")
     keyword = input("Please enter the key string: ")
-    text.lower().split(" ")
+    text_two = text.lower().replace(" ", "")
+    keyword_two = keyword.lower()
+    text_converted = ""
+    for x in range(len(text_two)):
+        number = alphabet.index(text_two[x])
+        number_key = alphabet.index(keyword_two[x % len(keyword_two)])
+        converted_number = number + number_key
+        text_converted = text_converted + alphabet[converted_number % 26]
+    print(text_converted)
+ 
